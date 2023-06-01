b6223fab937d589961c59c074dfb8213478af2e5
aravind-etagi <aravindetagi@gmail.com>
2023-06-01 22:28:19 +0530

    modified the script

--- tomcat_install.sh
+++ tomcat_install.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 TOMCAT_LINK="https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.9/bin/apache-tomcat-10.1.9.tar.gz"
+dir_name="apache-tomcat-10.1.9"
 
-sudo yum update -y > /dev/null
-sudo yum install java git -y > /dev/null
+sudo yum update -y
+sudo yum install java git wget -y
 
 sudo cd /opt
 sudo rm -rf apache-tomcat*
 sudo wget $TOMCAT_LINK
-tar_name=`echo $TOMCAT_LINK | awk -F "/" {print$NF}`
-sudo tar -xvzf $tar_name
-sudo rm -rf $tar_name
 
-dir_name=`echo $tar_name | awk -F ".tar" {print$1}`
+sudo tar -xvzf $dir_name.tar.gz
+sudo rm -rf $dir_name.tar.gz
 
 sudo git clone https://github.com/aravind-etagi/tomcat-config.git
-sudo cp ./tomcat-config/context.xml /opt/$dir_name/webapps/manager/META-INF/context.xml
-sudo cp ./tomcat-config/context.xml /opt/$dir_name/webapps/host-manager/META-INF/context.xml
-sudo cp ./tomcat-config/tomcat-users.xml /opt/$dir_name/conf/tomcat-users.xml
+sudo cp tomcat-config/context.xml /opt/$dir_name/webapps/manager/META-INF/context.xml
+sudo cp tomcat-config/context.xml /opt/$dir_name/webapps/host-manager/META-INF/context.xml
+sudo cp tomcat-config/tomcat-users.xml /opt/$dir_name/conf/tomcat-users.xml
 sudo rm -rf tomcat-config
 
 
-cd /opt/$dir_name
+sudo cd /opt/$dir_name
 
 sudo sh bin/shartup.sh
