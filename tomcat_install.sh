03e5ad9f3eb118f2e2eecbf916002f1a2f36bd7e
aravind-etagi <aravindetagi@gmail.com>
2023-06-01 22:45:37 +0530

    fixing bugs

--- tomcat_install.sh
+++ tomcat_install.sh
@@ -16,10 +16,7 @@ sudo rm -rf $dir_name.tar.gz
 sudo git clone https://github.com/aravind-etagi/tomcat-config.git
 sudo cp tomcat-config/context.xml /opt/$dir_name/webapps/manager/META-INF/context.xml
 sudo cp tomcat-config/context.xml /opt/$dir_name/webapps/host-manager/META-INF/context.xml
-sudo cp tomcat-config/tomcat-users.xml /opt/$dir_name/conf/tomcat-users.xml
+sudo cp tomcat-config/tomcat-user.xml /opt/$dir_name/conf/tomcat-user.xml
 sudo rm -rf tomcat-config
 
-
-sudo cd /opt/$dir_name
-
-sudo sh bin/shartup.sh
+sudo sh /opt/$dir_name/bin/startup.sh
