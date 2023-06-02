a278ccfeb6cd84c361aad7d541bfece78af8ca2a
aravind-etagi <aravindetagi@gmail.com>
2023-06-02 06:27:11 +0530

    added mv command

--- tomcat_install.sh
+++ tomcat_install.sh
@@ -6,12 +6,13 @@ dir_name="apache-tomcat-10.1.9"
 sudo yum update -y
 sudo yum install java git wget -y
 
-sudo cd /opt
+
 sudo rm -rf apache-tomcat*
 sudo wget $TOMCAT_LINK
 
 sudo tar -xvzf $dir_name.tar.gz
 sudo rm -rf $dir_name.tar.gz
+sudo mv $dir_name /opt
 
 sudo git clone https://github.com/aravind-etagi/tomcat-config.git
 sudo cp tomcat-config/context.xml /opt/$dir_name/webapps/manager/META-INF/context.xml
